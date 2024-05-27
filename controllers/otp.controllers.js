06acb191509b547b1f27989a8e968de2bf21bd9e
Muhammad 'Ammar Afif <amarafif230@gmail.com>
2024-05-27 11:00:20 +0700

    feat: smtp config for send otp

--- controllers/otp.controllers.js
+++ controllers/otp.controllers.js
@@ -1,88 +1,106 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const nodemailer = require('../libs/nodemailer');
-const otpGenerator = require('otp-generator');
+const nodemailer = require("../libs/nodemailer");
+const otpGenerator = require("otp-generator");
 
 module.exports = {
-    createUpdateotp: async (account_id, nama, email, res) => { // Menambahkan respon sebagai argumen
-        try {
-            //generate otp
-            const otpValue = otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false });
-            let existingOTP = null; // Deklarasi existingOTP
-            existingOTP = await prisma.otp.findUnique({
-                where: {
-                    account_id // Cari entri OTP berdasarkan account_id
-                }
-            });
+	createUpdateotp: async (account_id, nama, email, res) => {
+		// Menambahkan respon sebagai argumen
+		try {
+			//generate otp
+			const otpValue = otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false });
+			let existingOTP = null; // Deklarasi existingOTP
+			existingOTP = await prisma.otp.findUnique({
+				where: {
+					account_id, // Cari entri OTP berdasarkan account_id
+				},
+			});
 
-            if (existingOTP) {
-                // Jika entri OTP sudah ada, lakukan pembaruan
-                await prisma.otp.update({
-                    where: {
-                        account_id: account_id
-                    },
-                    data: {
-                        otp: otpValue // Update nilai OTP yang baru
-                    }
-                });
-            }else{
-            // Jika entri OTP belum ada, buat entri baru
-            existingOTP = await prisma.otp.create({
-                data: {
-                    otp: otpValue,
-                    account: {
-                        connect: {
-                            account_id: account_id // Hubungkan dengan account yang baru saja dibuat
-                        }
-                    }
-                }
-            });
-            }
-            
-            // Kirim OTP ke email pengguna
-            const html = `<p>Hi ${nama}, ini adalah OTP Anda: <strong>${otpValue}</strong></p>`;
-            await nodemailer.sendEmail(email, 'One-Time Password (OTP)', html);
-            
-            //menghapus otp setelah 5 menit dan belum diverifikasi
-            setTimeout(async (req, res, next) => {
-                try {
-            //         //mencari otp yang sesuai dengan id account dan belum diverifikasi
-                    const otpEntry = await prisma.account.findUnique({
-                        where: {
-                            account_id: account_id,
-                            is_verified: false
-                        }
-                    });
-                    if (otpEntry) {
-                        //hapus otp jika ditemukan
-                        await prisma.otp.update({
-                            where: {
-                                account_id: otpEntry.account_id
-                            },
-                            data: {
-                                otp: null
-                            }
-                        });
+			if (existingOTP) {
+				// Jika entri OTP sudah ada, lakukan pembaruan
+				await prisma.otp.update({
+					where: {
+						account_id: account_id,
+					},
+					data: {
+						otp: otpValue, // Update nilai OTP yang baru
+					},
+				});
+			} else {
+				// Jika entri OTP belum ada, buat entri baru
+				existingOTP = await prisma.otp.create({
+					data: {
+						otp: otpValue,
+						account: {
+							connect: {
+								account_id: account_id, // Hubungkan dengan account yang baru saja dibuat
+							},
+						},
+					},
+				});
+			}
 
-                        console.log(`OTP entry for account ${account_id} successfully deleted.`);
-                    } else {
-                        console.log(`No unverified OTP entry found for user ${account_id}.`);
-                    }
-                } catch (err) {
-                    console.error("Error while deleting OTP : ", err.message);
-                }
-            }, 300000);
+			// Kirim OTP ke email pengguna
+			const html = `<div style="font-family: Arial, sans-serif; text-align: center; color: #333;">			
+                            <h2 style="color: #333;">Welcome to CourseWay!</h2>
+                            <p style="color: #555; line-height: 1.6;">
+                                Hi ${nama}, Thank you for choosing CourseWay as your platform to explore and share knowledge. To complete your registration and unlock the power of knowledge, please verify your email by entering the OTP below:
+                            </p>
+                        
+                            <div style="background-color: #f4f4f4; padding: 15px; border-radius: 8px; margin: 20px 0;">
+                                <h3 style="margin-bottom: 10px; color: #333;">Your One-Time Passcode (OTP):</h3>
+                                <p style="font-size: 24px; color: #333; padding: 10px; background-color: #fff; border-radius: 5px;">${otpValue}</p>
+                            </div>
+                        
+                            <p style="line-height: 1.6; color: #888;">
+                                If you didn't request this OTP, you can safely ignore this email. Otherwise, welcome aboard! Explore the vast world of knowledge with CourseWay.
+                            </p>
+                        
+                            <p style="color: #555; margin-top: 30px;">
+                                Best regards,<br/>
+                                The CourseWay Team
+                            </p>
+                        </div>`;
+			await nodemailer.sendEmail(email, "🔔 One-Time Password (OTP)", html);
 
+			//menghapus otp setelah 5 menit dan belum diverifikasi
+			setTimeout(async (req, res, next) => {
+				try {
+					//         //mencari otp yang sesuai dengan id account dan belum diverifikasi
+					const otpEntry = await prisma.account.findUnique({
+						where: {
+							account_id: account_id,
+							is_verified: false,
+						},
+					});
+					if (otpEntry) {
+						//hapus otp jika ditemukan
+						await prisma.otp.update({
+							where: {
+								account_id: otpEntry.account_id,
+							},
+							data: {
+								otp: null,
+							},
+						});
 
+						console.log(`OTP entry for account ${account_id} successfully deleted.`);
+					} else {
+						console.log(`No unverified OTP entry found for user ${account_id}.`);
+					}
+				} catch (err) {
+					console.error("Error while deleting OTP : ", err.message);
+				}
+			}, 300000);
 
-            // // Mengembalikan existingOTP dalam respons HTTP 200
-            // return res.status(200).json({
-            //     status: true,
-            //     message: 'Proses OTP berhasil',
-            //     data: existingOTP
-            // });
-        } catch (err) {
-            console.error('error anda pada otp',err.message);
-        }
-    }
+			// // Mengembalikan existingOTP dalam respons HTTP 200
+			// return res.status(200).json({
+			//     status: true,
+			//     message: 'Proses OTP berhasil',
+			//     data: existingOTP
+			// });
+		} catch (err) {
+			console.error("error anda pada otp", err.message);
+		}
+	},
 };
--- libs/nodemailer.js
+++ libs/nodemailer.js
@@ -1,61 +1,74 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
-const {google} =  require('googleapis');
-// const ejs = require('ejs');
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const ejs = require("ejs");
 
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REFRESH_TOKEN, GOOGLE_SENDER_EMAIL, SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS } = process.env;
 
-const {GOOGLE_REFRESH_TOKEN,
-    GOOGLE_SENDER_EMAIL,
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET} = process.env;
+const oauth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET);
 
-    
-const oauth2Client = new google.auth.OAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET 
- );
-     
+// Set credentials by refresh token
+if (GOOGLE_REFRESH_TOKEN) {
+	oauth2Client.setCredentials({ refresh_token: GOOGLE_REFRESH_TOKEN });
+}
 
+const createTransporter = async () => {
+	if (GOOGLE_REFRESH_TOKEN) {
+		const accessToken = await oauth2Client.getAccessToken();
 
-//set credentials by refresh token
-oauth2Client.setCredentials({refresh_token:GOOGLE_REFRESH_TOKEN});
-
-module.exports = {
-        sendEmail : async (to, subject, html)=>{
-        const accesToken = await oauth2Client.getAccessToken();
-        // console.log(GOOGLE_REFRESH_TOKEN);
-        // console.log(accesToken);
-        
-        const transport = nodemailer.createTransport({
-            service: 'gmail',
-            auth : {
-            type : 'OAuth2',
-            user: GOOGLE_SENDER_EMAIL,
-            clientId: GOOGLE_CLIENT_ID,
-            clientSecret: GOOGLE_CLIENT_SECRET,
-            refreshToken: GOOGLE_REFRESH_TOKEN,
-            accessToken: accesToken
-            }
-        });
-        transport.sendMail({to, subject, html});
-        
-    },
-
-    getHtml : (fileName, data)=>{
-        return new Promise ((resolve, reject)=>{
-            const path = `${__dirname}/../views/${fileName}`;
-            ejs.renderFile(path, data, (err, data)=>{
-                if(err){
-                    return reject(err);
-                }
-                return resolve(data);
-            })
-        })
-    }
-    
-
+		return nodemailer.createTransport({
+			service: "gmail",
+			auth: {
+				type: "OAuth2",
+				user: GOOGLE_SENDER_EMAIL,
+				clientId: GOOGLE_CLIENT_ID,
+				clientSecret: GOOGLE_CLIENT_SECRET,
+				refreshToken: GOOGLE_REFRESH_TOKEN,
+				accessToken: accessToken.token,
+			},
+		});
+	} else {
+		return nodemailer.createTransport({
+			host: SMTP_HOST,
+			port: SMTP_PORT,
+			secure: SMTP_PORT == 465, // true for 465, false for other ports
+			auth: {
+				user: SMTP_USER,
+				pass: SMTP_PASS,
+			},
+		});
+	}
 };
 
+module.exports = {
+	sendEmail: async (to, subject, html) => {
+		try {
+			const transporter = await createTransporter();
+			const mailOptions = {
+				from: GOOGLE_SENDER_EMAIL || SMTP_USER, // Sender address
+				to: to, 
+				subject: subject, 
+				html: html,
+			};
 
+			const result = await transporter.sendMail(mailOptions);
+			console.log("Email sent: %s", result.messageId);
+			return result;
+		} catch (error) {
+			console.error("Error sending email:", error);
+			throw error;
+		}
+	},
 
-
+	getHtml: (fileName, data) => {
+		return new Promise((resolve, reject) => {
+			const path = `${__dirname}/../views/${fileName}`;
+			ejs.renderFile(path, data, (err, str) => {
+				if (err) {
+					return reject(err);
+				}
+				return resolve(str);
+			});
+		});
+	},
+};
